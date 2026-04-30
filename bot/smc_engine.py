936425a0e1113d76c12849ff7e70338a248b2aa4
TechSolute <pharshiesammy@gmail.com>
2026-04-30 20:34:18 +0100

    Update smc_engine.py

--- bot/smc_engine.py
+++ bot/smc_engine.py
@@ -3,68 +3,105 @@ import numpy as np
 from loguru import logger
 
 class SMCEngine:
-    """
-    Smart Money Concepts: Order Blocks, Fair Value Gaps, Break of Structure, Change of Character.
-    """
     def __init__(self, config):
         self.ob_lookback = config['indicators']['smc']['ob_lookback']
         self.fvg_min_gap = config['indicators']['smc']['fvg_min_gap_pct'] / 100
 
-    def detect_order_blocks(self, df: pd.DataFrame):
-        # Simplified: bullish OB = last bearish candle before a break higher
+    def find_swing_points(self, df: pd.DataFrame, window=3):
+        """
+        Return swing highs/lows index lists.
+        """
+        high_cond = (df['high'] > df['high'].shift(1)) & (df['high'] > df['high'].shift(-1)) & \
+                    (df['high'] > df['high'].rolling(window).max().shift(1))
+        low_cond = (df['low'] < df['low'].shift(1)) & (df['low'] < df['low'].shift(-1)) & \
+                   (df['low'] < df['low'].rolling(window).min().shift(1))
+        swing_highs = df.index[high_cond].tolist()
+        swing_lows = df.index[low_cond].tolist()
+        return swing_highs, swing_lows
+
+    def detect_bos(self, df, swing_highs, swing_lows):
+        """
+        Break of Structure: price exceeds previous swing high/low.
+        """
+        bos = []
+        last_high = None
+        last_low = None
+        for i in range(1, len(df)):
+            idx = df.index[i]
+            # bullish BOS
+            if last_high and df['close'].iloc[i] > last_high:
+                bos.append({'index': idx, 'type': 'BOS_bull', 'level': last_high})
+                last_high = df['high'].iloc[i]
+            # bearish BOS
+            if last_low and df['close'].iloc[i] < last_low:
+                bos.append({'index': idx, 'type': 'BOS_bear', 'level': last_low})
+                last_low = df['low'].iloc[i]
+            # update swing levels
+            if idx in swing_highs:
+                last_high = df.loc[idx, 'high']
+            if idx in swing_lows:
+                last_low = df.loc[idx, 'low']
+        return bos
+
+    def detect_choch(self, df, swing_highs, swing_lows):
+        """
+        Change of Character: break of a minor swing structure.
+        """
+        choch = []
+        # Check for bearish CHOCH (break of higher low)
+        for i in range(2, len(swing_lows)):
+            recent_low = swing_lows[i-1]
+            if df.loc[recent_low, 'low'] > df.loc[swing_lows[i-2], 'low']:  # higher low
+                # see if later price breaks below that higher low
+                mask = df.index > recent_low
+                break_idx = df.index[mask & (df['low'] < df.loc[recent_low, 'low'])]
+                if not break_idx.empty:
+                    choch.append({'index': break_idx[0],
+                                  'type': 'CHOCH_bear'})
+                    break
+        # Check for bullish CHOCH (break of lower high)
+        for i in range(2, len(swing_highs)):
+            recent_high = swing_highs[i-1]
+            if df.loc[recent_high, 'high'] < df.loc[swing_highs[i-2], 'high']:
+                mask = df.index > recent_high
+                break_idx = df.index[mask & (df['high'] > df.loc[recent_high, 'high'])]
+                if not break_idx.empty:
+                    choch.append({'index': break_idx[0],
+                                  'type': 'CHOCH_bull'})
+                    break
+        return choch
+
+    def detect_order_blocks(self, df):
         obs = []
-        if len(df) < self.ob_lookback + 2: return obs
-        for i in range(self.ob_lookback, len(df)-1):
-            # Break of previous high (BOS)
-            if df['high'].iloc[i] > df['high'].iloc[i-1]:
-                # Find last bearish candle within lookback
-                for j in range(i-1, max(i-self.ob_lookback, 0), -1):
-                    if df['close'].iloc[j] < df['open'].iloc[j]:
-                        ob = {'index': df.index[j], 'top': df['open'].iloc[j], 'bottom': df['low'].iloc[j],
-                              'type': 'bullish', 'origin': df.index[j]}
-                        obs.append(ob)
-                        break
-            # Break of previous low (bearish OB)
-            if df['low'].iloc[i] < df['low'].iloc[i-1]:
-                for j in range(i-1, max(i-self.ob_lookback, 0), -1):
-                    if df['close'].iloc[j] > df['open'].iloc[j]:
-                        ob = {'index': df.index[j], 'top': df['high'].iloc[j], 'bottom': df['close'].iloc[j],
-                              'type': 'bearish', 'origin': df.index[j]}
-                        obs.append(ob)
-                        break
+        for i in range(self.ob_lookback+1, len(df)):
+            # bullish OB: bearish candle followed by price rise breaking its high
+            if df['close'].iloc[i] > df['open'].iloc[i]:
+                if df['close'].iloc[i-1] < df['open'].iloc[i-1]:
+                    ob_high = df['open'].iloc[i-1]
+                    ob_low = df['low'].iloc[i-1]
+                    obs.append({'index': df.index[i-1], 'top': ob_high, 'bottom': ob_low,
+                                'type': 'bullish', 'origin': df.index[i-1]})
         return obs
 
-    def detect_fvg(self, df: pd.DataFrame):
+    def detect_fvg(self, df):
         fvgs = []
         for i in range(1, len(df)-1):
-            prev_high, prev_low = df['high'].iloc[i-1], df['low'].iloc[i-1]
-            next_low, next_high = df['low'].iloc[i+1], df['high'].iloc[i+1]
+            prev_high = df['high'].iloc[i-1]
+            prev_low = df['low'].iloc[i-1]
+            next_low = df['low'].iloc[i+1]
+            next_high = df['high'].iloc[i+1]
             gap_up = next_low - prev_high
             gap_down = prev_low - next_high
             if gap_up > 0 and (gap_up / prev_high) > self.fvg_min_gap:
                 fvgs.append({'index': df.index[i], 'type': 'bullish', 'top': next_low, 'bottom': prev_high})
-            elif gap_down > 0 and (gap_down / prev_high) > self.fvg_min_gap:
+            if gap_down > 0 and (gap_down / prev_high) > self.fvg_min_gap:
                 fvgs.append({'index': df.index[i], 'type': 'bearish', 'top': prev_low, 'bottom': next_high})
         return fvgs
 
-    def detect_bos_choch(self, df: pd.DataFrame):
-        bos = []
-        choch = []
-        swing_highs = (df['high'] > df['high'].shift(1)) & (df['high'] > df['high'].shift(-1))
-        swing_lows = (df['low'] < df['low'].shift(1)) & (df['low'] < df['low'].shift(-1))
-        for idx in df.index[2:-2]:
-            if swing_highs.loc[idx]:
-                bos.append({'index': idx, 'type': 'BOS_high'})
-            if swing_lows.loc[idx]:
-                bos.append({'index': idx, 'type': 'BOS_low'})
-        # CHOCH: break of last swing low/high
-        # Simplified: later
-        return bos, choch
-
-    def liquidity_zones(self, df: pd.DataFrame):
-        # Equal highs/lows clusters
-        highs = df['high'].value_counts()
-        lows = df['low'].value_counts()
-        lq_high = highs[highs > 2].index.tolist()  # levels touched 3+ times
-        lq_low = lows[lows > 2].index.tolist()
+    def liquidity_zones(self, df):
+        # round to 2 decimals
+        high_counts = df['high'].round(2).value_counts()
+        low_counts = df['low'].round(2).value_counts()
+        lq_high = high_counts[high_counts > 2].index.tolist()
+        lq_low = low_counts[low_counts > 2].index.tolist()
         return {'high_liquidity': lq_high, 'low_liquidity': lq_low}
